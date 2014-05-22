5d9f2060c1f8f1d631ce15b9e15ca85cd2dc562f
saden1 <saden1@gmail.com>
2014-05-22 13:19:59 -0700

    added event bus example

---
+++ playground-hk2/src/test/java/com/fitbur/playground/hk2/event/TopicPublisherNGTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2014 Fitbur.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fitbur.playground.hk2.event;
+
+import javax.inject.Inject;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.jvnet.testing.hk2testng.HK2;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Sharmarke Aden
+ */
+@HK2
+public class TopicPublisherNGTest {
+
+    @Inject
+    EventService sut;
+
+    @Test
+    public void assertInjection() {
+        assertThat(sut).isNotNull();
+    }
+
+    @Test
+    public void assertPublishing() {
+        sut.publish("publish");
+    }
+
+    @Test
+    public void assertSubscription() {
+        sut.publish("subscribe");
+        assertThat(sut.getMessage()).isEqualTo("subscribe");
+    }
+
+}
